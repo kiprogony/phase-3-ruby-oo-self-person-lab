68f9f417559e60eb98cf54a5573a938334ff727d
Daryl Rogony <darylrogony@gmail.com>
2022-06-11 06:46:48 +0300

    commit tree 27604175a84aec0398221f984f8a4fc09d5efccc

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,54 @@
 # your code goes here
+
+class Person
+  attr_accessor :bank_account
+  attr_reader :name, :hygiene, :happiness
+
+  def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+  end
+
+  def happiness=(number)
+    @happiness = if number > 10
+                    10
+                  elsif number < 0
+                    0
+                  else
+                    number
+                  end
+  end
+
+  def hygiene=(number)
+    @hygiene = if number > 10
+      10
+    elsif number < 0
+      0
+    else
+      number
+    end
+  end
+
+  def clean?
+    self.hygiene > 7
+  end
+
+  def happy?
+    self.happiness > 7
+  end
+  
+  def get_paid(amount)
+    self.bank_account += amount
+    "all about the benjamins"
+  end
+
+  def take_bath
+    self.hygiene +=4
+    "♪ Rub-a-dub just relaxing in the tub ♫"
+  end
+end
+
+# person = Person.new
+# person.name
